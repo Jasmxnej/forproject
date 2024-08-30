484e53a26e5708628f72c121ddb2a928f193b9c9
Cxttonn <phitchayapha_b@cmu.ac.th>
2024-08-30 16:07:30 +0700

    Update Tailwind in App.vue
    Add Tailwind and Delete Style scope

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,14 @@
 <template>
   <div id="layout">
     <header>
-      <div class="wrapper">
+      <div class="p-8">
         <nav>
-          <RouterLink :to="{ name: 'list-view' }">Home</RouterLink>
+          <RouterLink
+            :to="{ name: 'list-view' }"
+            class="font-bold text-gray-800 hover:text-green-500 transition"
+          >
+            Home
+          </RouterLink>
         </nav>
       </div>
     </header>
@@ -14,58 +19,3 @@
   </div>
 </template>
 
-<style scoped>
-#layout {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-
-h2 {
-  font-size: 20px;
-}
-
-@keyframes yellofade {
-  from {
-    background-color: yellow;
-  }
-  to {
-    background-color: transparent;
-  }
-}
-
-#flashMessage {
-  animation: yellofade 3s ease-in-out;
-  position: fixed;
-  top: 20px;
-  left: 50%;
-  transform: translateX(-50%);
-  background-color: yellow;
-  padding: 10px;
-  border-radius: 4px;
-  z-index: 1000;
-}
-
-.flash-message {
-  background-color: #f0ad4e;
-  color: black;
-  padding: 10px 20px;
-  border-radius: 5px;
-  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-}
-</style>
