8a9dec3c1c70806b498c13532fc1dc108be93b59
Cxttonn <phitchayapha_b@cmu.ac.th>
2024-08-30 15:55:05 +0700

    Update Tailwind.config.ts, create style.css

--- src/components/CountryContent.vue
+++ src/components/CountryContent.vue
@@ -32,67 +32,30 @@ watchEffect(() => {
 
 <template>
   <tbody>
-    <tr v-for="event in events" :key="event.id" class="event-row">
-      <td class="cell flag-cell">
-        <img :src="event.flag_url" alt="Flag" class="flag-image" />
+    <tr v-for="event in events" :key="event.id" class="hover:bg-gray-50">
+      <td class="px-4 py-2 border text-center">
+        <img :src="event.flag_url" alt="Flag" class="w-12 h-auto mx-auto" />
       </td>
-      <td class="cell name-cell">
+      <td class="px-4 py-2 border">
         <RouterLink
           :to="{ name: 'list-view', params: { id: event.id } }"
-          class="event-link"
+          class="text-600 hover:underline"
         >
           {{ event.name }}
         </RouterLink>
       </td>
-      <td class="cell medal-cell">
+      <td class="px-4 py-2 border text-center">
         {{ event.medals_by_sport?.until_2024?.total?.gold || 0 }}
       </td>
-      <td class="cell medal-cell">
+      <td class="px-4 py-2 border text-center">
         {{ event.medals_by_sport?.until_2024?.total?.silver || 0 }}
       </td>
-      <td class="cell medal-cell">
+      <td class="px-4 py-2 border text-center">
         {{ event.medals_by_sport?.until_2024?.total?.bronze || 0 }}
       </td>
-      <td class="cell medal-cell">{{ event.total_medals }}</td>
+      <td class="px-4 py-2 border text-center">{{ event.total_medals }}</td>
     </tr>
   </tbody>
 </template>
 
-<style scoped>
-.event-row:hover {
-  background-color: #f9fafb;
-}
 
-.cell {
-  padding: 0.5rem 1rem;
-  border: 1px solid #e5e7eb;
-}
-
-.flag-cell {
-  text-align: center;
-}
-
-.flag-image {
-  width: 3rem;
-  height: auto;
-  margin-left: auto;
-  margin-right: auto;
-}
-
-.name-cell {
-  text-align: center;
-}
-
-.event-link {
-  color: #374151;
-  text-decoration: none;
-}
-
-.event-link:hover {
-  text-decoration: underline;
-}
-
-.medal-cell {
-  text-align: center;
-}
-</style>
